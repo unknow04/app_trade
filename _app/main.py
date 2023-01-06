5251f7b07466080bb1b7822e43d3462182585f2d
unknow04 <unknow04@mail.ru>
2023-01-06 16:02:02 +0300

    add settings database

--- _app/main.py
+++ _app/main.py
@@ -1,29 +1,72 @@
+from kivy.lang import Builder
 from kivymd.app import MDApp
+from kivymd.uix.menu import MDDropdownMenu
 from kivymd.uix.screenmanager import MDScreenManager
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.boxlayout import MDBoxLayout
+
+import menu
+from settings_db import SettingData
 
 
 class TitleScreen(MDScreen):
     """класс главного окна"""
-    pass
+    menu = menu.MENU_RUS
+    connect_status = False
+
+    def show_password(self):
+        pass
+
+    def connect_icon(self):
+        if self.connect_status:
+            return "lan-connect"
+        else:
+            return "lan-disconnect"
 
 
 class SettingScreen(MDScreen):
     """класс окна настроек"""
-    pass
+    db = SettingData()
+
+    def save_settings(self, *args):
+        if args[0] and args[1] != "":
+            self.ids['text4'].hint_text = ""
+            self.ids['text5'].hint_text = ""
+            self.db.save_data(*args)
+
+    def load_settings(self):
+        data = self.db.get_data()
+        if data:
+            return data
+        else:
+            return (0, "None data", "None data")
 
 
 class RegistrationScreen(MDScreen):
     """класс формы регистрации"""
-    pass
+
+    def read_setting(self):
+        pass
 
 
 class TradeApp(MDApp):
     """Главное приложение"""
 
+    def __int__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def callback(self):
+        pass
+
     def build(self):
         sm = MDScreenManager()
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "LightBlue"
         sm.add_widget(TitleScreen(name='title'))
         sm.add_widget(SettingScreen(name='settings'))
         sm.add_widget(RegistrationScreen(name='registration_form'))
         return sm
+
+
+if __name__ == '__main__':
+    TradeApp().run()
---
+++ _app/settings_db.py
@@ -0,0 +1,36 @@
+import sqlite3 as sq
+
+query_setting = """CREATE TABLE IF NOT EXISTS settings(id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                                       host VARCHAR, port VARCHAR);"""
+
+
+class SettingData:
+    def __init__(self):
+        self.conn = sq.connect("../database/settings_db.db")
+        self.cur = self.conn.cursor()
+        self.cur.execute(query_setting)
+        self.conn.commit()
+
+    def delete_data(self):
+        self.cur.execute("""DELETE FROM settings;""")
+        self.conn.commit()
+
+    def get_data(self):
+        data = self.cur.execute("""SELECT * FROM settings;""")
+        data = data.fetchone()
+        return data
+
+    def save_data(self, *args):
+        try:
+            self.delete_data()
+        except:
+            pass
+        self.cur.execute("""INSERT INTO settings(host, port) VALUES(?, ?);""", args)
+        self.conn.commit()
+
+
+
+if __name__ == '__main__':
+    db = SettingData()
+    db.save_data("128.0.0.1", 5003)
+    db.get_data()
